6b2d927c73dd9a74dbc7285fde32e76dec443343
Svartalfheim3528 <jmwashenberger@live.com>
2015-12-01 20:09:51 -0600

    Update Plane.h

--- Plane.h
+++ Plane.h
@@ -10,12 +10,18 @@ namespace Plane
 		public:
 			string DepartCity, ArrivalCity;
 			int DepartTime, ArrivalTime;
-			void fillSeats(int rows, int columns)  //In the future may need to add parameters for imported files
+			void fillSeats(int rows, int columns);  //In the future may need to add parameters for imported files
+			void setSeat(int m, int n, Passenger P);
+			void setDepartCity(string City);
+			void setDepartTime(int Time);
+			void setArrivalCity(string City);
+			void setArrivalTime(int Time);
+			
 		private:
 			Flight **seats;  //Array of type Flight because seat classes are all of type Flight
 			//We won't need to do anything special for this, each flight class(Economy, economy plus etc)
 			//will be of type Flight, and therefore won't need to seperate them.
-	}
+	};
 }
 
 
